9be0a8cb99e79465624d01f0e73da5ae1d08cc47
RachaponKjr <rachapon.navy@gmail.com>
2026-09-05 22:43:19 +0700

    fix

--- front-steaming/app/(dashboard)/dashboard/_components/dashboard-page.tsx
+++ front-steaming/app/(dashboard)/dashboard/_components/dashboard-page.tsx
@@ -205,9 +205,7 @@ export default function DashboardPage({ params }: DashboardProps) {
         if (isMounted) {
           setLivekitToken(data.token);
           // หาก backend ส่งค่า wsUrl เป็น http:// หรือโดเมน docker ให้แปลงเป็น ws://127.0.0.1:7880
-          const validWsUrl = data.wsUrl?.startsWith("ws")
-            ? data.wsUrl
-            : "ws://127.0.0.1:7880";
+          const validWsUrl = "wss://livekit.zimonds.com";
           setWsUrl(validWsUrl);
         }
       })
