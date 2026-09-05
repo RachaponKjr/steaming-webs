2462b16a02a18e63672ef334c97634a5efecf8e0
RachaponKjr <rachapon.navy@gmail.com>
2026-09-05 11:14:08 +0700

    fix

--- front-steaming/proxy.ts
+++ front-steaming/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. ดึง Token จาก Cookie
   const token = request.cookies.get("admin_token")?.value;
   const { pathname } = request.nextUrl;
